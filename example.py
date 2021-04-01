7e4101ca7fdc81e3b0e4ba047220bc75dd4484e9
Michael <56702056+MichaelDarmoutomo@users.noreply.github.com>
2021-04-01 10:12:01 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,16 @@
-temperature = 10
+def add(a, b):
+    return a + b
 
-def print_temperature():
-    print(temperature)
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+def subtract(a, b):
+    return a + b  # <--- fix this 
+
+
+# uncomment the following test
+#def test_subtract():
+#    assert subtract(2, 3) == -1
